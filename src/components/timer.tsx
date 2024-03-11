5ed7d9e19fd0ca905fba3bca2003be33f2597160
Mateo <rosenmatt1@gmail.com>
2024-03-10 23:12:16 -0600

    Added timer component

---
+++ src/components/timer.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <h1>Current Time:</h1>
+      <p>{time.toLocaleTimeString()}</p>
+    </div>
+  );
+}
+
+export default Clock;
+
