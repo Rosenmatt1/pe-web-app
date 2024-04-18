2670ab327ba1030203ddbab68461d1b0b57b93cb
Mateo <rosenmatt1@gmail.com>
2024-04-17 23:56:53 -0600

    Added an options variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Timer from './components/timer'
+import Dropdown from './components/dropdown'
 import './App.css';
+
+
+const options = ['Option 1', 'Option 2', 'Option 3'];
+
+const handleSelectOption = (option: string) => {
+    console.log(`Selected option: ${option}`);
+  };
 
 function App() {
   return (
     <div className="peapp">
+         <Dropdown options={options} onSelectOption={handleSelectOption} />
         <Timer/>
     </div>
   );
---
+++ src/components/dropdown.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+interface DropdownProps {
+  options: string[];
+  onSelectOption: (option: string) => void;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ options, onSelectOption }) => {
+  const [selectedOption, setSelectedOption] = useState<string | any>(null);
+
+  const handleOptionSelect = (option: string) => {
+    setSelectedOption(option);
+    onSelectOption(option);
+  };
+
+  return (
+    <div>
+      <select
+        value={selectedOption}
+        onChange={(e) => handleOptionSelect(e.target.value)}
+      >
+        <option value="">Select an option</option>
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Dropdown;
