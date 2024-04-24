a8ddb1808b73b164f3c311fba36879ba57c21498
Mateo <rosenmatt1@gmail.com>
2024-04-24 00:29:46 -0600

    Installed react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import Timer from './components/timer'
 import Dropdown from './components/dropdown'
 import './App.css';
 
+
+// Define the type for the context value
+interface MyContextType {
+  value1: string;
+  setValue1: React.Dispatch<React.SetStateAction<string>>;
+  value2: string;
+  setValue2: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// Create the context
+const MyContext = createContext<MyContextType | undefined>(undefined);
 
 const options = ['Stretching', "Hanging", 'Pumping', 'Clamping', ];
 
